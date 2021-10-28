9c79892e2f247ee88b14e4a3a0befda3c76af99a
DanishSven <stefan.hall@student.uts.edu.au>
2021-10-28 22:32:50 +1100

    Added csv uploaded to streamlit app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,16 @@
-from ..src.data import Dataset
-from ..src.datetime_part import DateColumn
-from ..src.numeric import NumericColumn
-from ..src.text import TextColum
+# from src.data import Dataset
+# from src.datetime_part import DateColumn
+# from src.numeric import NumericColumn
+# from src.text import TextColum
 import streamlit as st
 import pandas as pd
  
-print('hi')
+st.title("Automatic Exploratory Data Analysis")
+st.write("Simply upload a csv and watch the data unfold..")
+
+try:
+    uploaded = st.file_uploader("Upload your .csv here:", ['csv'])
+    dataframe = pd.read_csv(uploaded)
+except:
+    st.error("Please upload a csv to begin")
+    st.stop()
