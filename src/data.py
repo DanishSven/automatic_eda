86fd15ccd56d314b4c2151abe1db85fc03c2c4fd
Matthew Moghaddam <matthew.moghaddam@student.uts.edu.au>
2021-11-02 08:24:47 +1100

    change 2 Nov

--- src/data.py
+++ src/data.py
@@ -4,145 +4,71 @@ import numpy as np
 
 
 @dataclass
-#class Dataset:
-#   name: str
-#   df: pd.DataFrame
-#   datetime_col: list
+class Dataset:
+   name: str
+   df: pd.DataFrame
+   datetime_col: list
 
-  class FileReader():
-
-    def __init__(self, file_name, data):
-        self.file_name = file_name  # initialize file_name attribute
-        self.data = data  # initialize data attribute
-
+    
     def get_name(self):
         """
       Return filename of loaded dataset
       """
-        return self.file_name
+        return None
 
     def get_n_rows(self):
         """
         Return number of rows of loaded dataset
       """
-        return self.data.shape[0]
+        return None
 
     def get_n_cols(self):
         """
         Return number of columns of loaded dataset
       """
-        return self.data.shape[1]
+        return None
 
     def get_cols_list(self):
         """
         Return list column names of loaded dataset
       """
-        return self.data.columns.values.tolist()
+        return None
 
     def get_cols_dtype(self):
         """
         Return dictionary with column name as keys and data type as values
       """
-        # get all column name as keys and data type as values
-        keys_values = self.data.dtypes.to_dict().items()
-
-        # convert data type value to string then return dictionary
-        dict = {key: str(value) for key, value in keys_values}
-
-        return dict
+        return None
 
     def get_n_duplicates(self):
         """
         Return number of duplicated rows of loaded dataset
       """
-        unique_df = self.data.drop_duplicates()  # get unique rows
-        # duplicated row = total number of rows - total number of unique rows
-        return self.data.shape[0] - unique_df.shape[0]
+        return None
 
     def get_n_missing(self):
         """
         Return number of rows with missing values of loaded dataset
       """
-        return self.data.isnull().sum().sum()
+        return None
 
     def get_head(self, n=5):
         """
         Return Pandas Dataframe with top rows of loaded dataset
       """
-        return self.data.head(n)
+        return None
 
     def get_tail(self, n=5):
         """
         Return Pandas Dataframe with bottom rows of loaded dataset
       """
-        return self.data.tail(n)
+        return None
 
     def get_sample(self, n=5):
         """
         Return Pandas Dataframe with random sampled rows of loaded dataset
       """
-        return self.data.sample(n)
-  
-    
-    #def get_name(self):
-     #   """
-      #Return filename of loaded dataset
-      #"""
-       # return None
-
-    #def get_n_rows(self):
-     #   """
-      #  Return number of rows of loaded dataset
-      #"""
-       # return None
-
-    #def get_n_cols(self):
-     #   """
-      #  Return number of columns of loaded dataset
-     # """
-      #  return None
-
-    #def get_cols_list(self):
-     #   """
-      #  Return list column names of loaded dataset
-      #"""
-       # return None
-
-    #def get_cols_dtype(self):
-     #   """
-      #  Return dictionary with column name as keys and data type as values
-      #"""
-       # return None
-
-    #def get_n_duplicates(self):
-     #   """
-      #  Return number of duplicated rows of loaded dataset
-      #"""
-       # return None
-
-    #def get_n_missing(self):
-     #   """
-      #  Return number of rows with missing values of loaded dataset
-      #"""
-       # return None
-
-    #def get_head(self, n=5):
-     #   """
-      #  Return Pandas Dataframe with top rows of loaded dataset
-      #"""
-       # return None
-
-    #def get_tail(self, n=5):
-     #   """
-      #  Return Pandas Dataframe with bottom rows of loaded dataset
-      #"""
-       # return None
-
-    #def get_sample(self, n=5):
-     #   """
-      #  Return Pandas Dataframe with random sampled rows of loaded dataset
-      #"""
-       # return None
+        return None
 
     def get_numeric_columns(self):
         """
