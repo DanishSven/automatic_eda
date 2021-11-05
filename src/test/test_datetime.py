aba9ec4319be75c666b223e484cbd02c55e3c484
DanishSven <stefan.hall@student.uts.edu.au>
2021-11-05 20:13:57 +1100

    changes to datetime_test

--- src/test/test_datetime.py
+++ src/test/test_datetime.py
@@ -23,7 +23,7 @@ notdate_test = DateColumn('Not Dates',notdate_test)
 
 class datetime_test(unittest.TestCase):
 
-    def get_unique_test(self):
+    def test_get_unique(self):
         unique_vals = 9
         unique_test = date_test.get_unique()
         notdate_unique = notdate_test.get_unique()
@@ -31,7 +31,7 @@ class datetime_test(unittest.TestCase):
         self.assertEqual(unique_vals,unique_test)
         self.assertEqual(unique_vals,notdate_unique)
 
-    def get_missing_test(self):
+    def test_get_missing_test(self):
         missing_vals = 1
         missing_test = date_test.get_missing()
         notdate_missing = notdate_test.get_missing
@@ -39,25 +39,25 @@ class datetime_test(unittest.TestCase):
         self.assertEqual(missing_vals,missing_test)
         self.assertEqual(missing_vals,notdate_missing)
 
-    # def get_weekend_test(self):
+    # def test_get_weekend_test(self):
 
-    # def get_weekday_test(self):
+    # def test_get_weekday_test(self):
 
-    # def get_future_test(self):
+    # def test_get_future_test(self):
     
-    # def get_empty_1900_test(self):
+    # def test_get_empty_1900_test(self):
 
-    # def get_empty_1970_test(self):
+    # def test_get_empty_1970_test(self):
 
-    # def get_min_test(self):
+    # def test_get_min_test(self):
 
-    # def get_max_test(self):
+    # def test_get_max_test(self):
 
-    # def get_barchart_test(self):
+    # def test_get_barchart_test(self):
 
-    # def get_frequent_test(self):
+    # def test_get_frequent_test(self):
 
-    # def construct_table(_testself):
+    # def test_construct_table(_testself):
 
 if __name__ == '__main__':
     unittest.main()
